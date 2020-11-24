21ab4fec83783d8dd02b84d4afae341bcac7122c
katus <sunkeran0818@outlook.com>
2020-11-24 09:07:34 +0800

    Add new model 'Union'.

---
+++ src/main/java/com/katus/model/Union.java
@@ -0,0 +1,118 @@
+package com.katus.model;
+
+import com.katus.entity.Feature;
+import com.katus.entity.Layer;
+import com.katus.entity.LayerMetadata;
+import com.katus.io.writer.LayerTextFileWriter;
+import com.katus.model.args.UnionArgs;
+import com.katus.util.*;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.sql.SparkSession;
+import org.locationtech.jts.geom.Geometry;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+/**
+ * @author Mengxiao Wang (wmx), Keran Sun (katus)
+ * @version 2.0, 2020-11-24
+ */
+@Slf4j
+public class Union {
+    public static void main(String[] args) throws Exception {
+        log.info("Setup Spark Session");
+        SparkSession ss = SparkUtil.getSparkSession();
+
+        log.info("Setup arguments");
+        UnionArgs mArgs = UnionArgs.initArgs(args);
+        if (mArgs == null) {
+            String msg = "Init Union Args failed, exit!";
+            log.error(msg);
+            throw new RuntimeException(msg);
+        }
+
+        log.info("Make layers");
+        Layer layer1 = InputUtil.makeLayer(ss, mArgs.getInput1(), Boolean.valueOf(mArgs.getHasHeader1()),
+                Boolean.valueOf(mArgs.getIsWkt1()), mArgs.getGeometryFields1().split(","), mArgs.getSeparator1(),
+                mArgs.getCrs1(), mArgs.getCharset1(), mArgs.getGeometryType1());
+        Layer layer2 = InputUtil.makeLayer(ss, mArgs.getInput2(), Boolean.valueOf(mArgs.getHasHeader2()),
+                Boolean.valueOf(mArgs.getIsWkt2()), mArgs.getGeometryFields2().split(","), mArgs.getSeparator2(),
+                mArgs.getCrs2(), mArgs.getCharset2(), mArgs.getGeometryType2());
+
+        log.info("Dimension check");
+        if (GeometryUtil.getDimensionOfGeomType(layer1.getMetadata().getGeometryType()) !=
+                GeometryUtil.getDimensionOfGeomType(layer2.getMetadata().getGeometryType())) {
+            String msg = "Two layers must have the same dimension, exit!";
+            log.error(msg);
+            throw new RuntimeException(msg);
+        }
+
+        log.info("Prepare calculation");
+        if (!mArgs.getCrs().equals(mArgs.getCrs1())) {
+            layer1 = layer1.project(CrsUtil.getByCode(mArgs.getCrs()));
+        }
+        if (!mArgs.getCrs().equals(mArgs.getCrs2())) {
+            layer2 = layer2.project(CrsUtil.getByCode(mArgs.getCrs()));
+        }
+        layer1 = layer1.index(14);
+        layer2 = layer2.index(14);
+
+        log.info("Start Calculation");
+        Layer layer = union(layer1, layer2);
+
+        log.info("Output result");
+        LayerTextFileWriter writer = new LayerTextFileWriter("", mArgs.getOutput());
+        writer.writeToFileByPartCollect(layer, Boolean.parseBoolean(mArgs.getNeedHeader()), false, true);
+
+        ss.close();
+    }
+
+    public static Layer union(Layer layer1, Layer layer2) {
+        LayerMetadata metadata1 = layer1.getMetadata();
+        LayerMetadata metadata2 = layer2.getMetadata();
+        int dimension = GeometryUtil.getDimensionOfGeomType(metadata1.getGeometryType());
+        String[] fields = FieldUtil.merge(metadata1.getFieldNames(), metadata2.getFieldNames());
+        JavaPairRDD<String, Feature> result = layer1.fullOuterJoin(layer2)
+                .flatMapToPair(fullPairItems -> {
+                    List<Tuple2<String, Feature>> resultList = new ArrayList<>();
+                    Feature f1 = null, f2 = null;
+                    Feature feature1 = null, feature2 = null;
+                    if (fullPairItems._2()._1().isPresent()) feature1 = fullPairItems._2()._1().get();
+                    if (fullPairItems._2()._2().isPresent()) feature2 = fullPairItems._2()._2().get();
+                    if (feature1 != null && feature2 != null && feature1.getGeometry().intersects(feature2.getGeometry())) {
+                        Geometry diff = GeometryUtil.breakGeometryCollectionByDimension(feature2.getGeometry().difference(feature1.getGeometry()), dimension);
+                        LinkedHashMap<String, Object> attributes = AttributeUtil.merge(fields, feature1.getAttributes(), feature2.getAttributes());
+                        f1 = new Feature(feature1.getFid(), attributes, feature1.getGeometry());
+                        if (!diff.isEmpty()) f2 = new Feature(feature2.getFid(), attributes, diff);
+                    } else {
+                        if (feature1 != null) {
+                            LinkedHashMap<String, Object> attributes1 = AttributeUtil.merge(fields, feature1.getAttributes(), new HashMap<>());
+                            f1 = new Feature(feature1.getFid(), attributes1, feature1.getGeometry());
+                        }
+                        if (feature2 != null) {
+                            LinkedHashMap<String, Object> attributes2 = AttributeUtil.merge(fields, new HashMap<>(), feature2.getAttributes());
+                            f2 = new Feature(feature2.getFid(), attributes2, feature2.getGeometry());
+                        }
+                    }
+                    if (f1 != null) resultList.add(new Tuple2<>(fullPairItems._1() + "#" + f1.getFid(), f1));
+                    if (f2 != null) resultList.add(new Tuple2<>(fullPairItems._1() + "#" + f2.getFid(), f2));
+                    return resultList.iterator();
+                })
+                .reduceByKey((feature1, feature2) -> {
+                    if (feature1 == null) return null;
+                    if (feature2 == null) return null;
+                    if (feature1.getGeometry().equals(feature2.getGeometry())) return feature1;
+                    if (feature1.getGeometry().intersects(feature2.getGeometry())) {
+                        Geometry geometry = GeometryUtil.breakGeometryCollectionByDimension(feature1.getGeometry().intersection(feature2.getGeometry()), dimension);
+                        return new Feature(feature1.getFid(), feature1.getAttributes(), geometry);
+                    } else return null;
+                })
+                .filter(pairItem -> pairItem._2() != null && pairItem._2().hasGeometry())
+                .cache();
+        return Layer.create(result, fields, metadata1.getCrs(), metadata1.getGeometryType(), result.count());
+    }
+}
---
+++ src/main/java/com/katus/model/args/UnionArgs.java
@@ -0,0 +1,90 @@
+package com.katus.model.args;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+/**
+ * @author Keran Sun (katus)
+ * @version 1.0, 2020-11-20
+ */
+@Getter
+@Setter
+@Slf4j
+public class UnionArgs {
+    @Option(name = "-output", usage = "输出文件路径", required = true)
+    private String output;
+
+    @Option(name = "-needHeader", usage = "输出文件是否含有标题行")
+    private String needHeader = "true";   // false, true
+
+    @Option(name = "-crs", usage = "运算过程的地理参考")
+    private String crs = "4326";   // 4326, 3857
+
+    @Option(name = "-input1", usage = "输入目标数据路径", required = true)
+    private String input1;
+    /**
+     * The below is only for text file
+     */
+    @Option(name = "-hasHeader1", usage = "输入目标数据是否含有标题行")
+    private String hasHeader1 = "false";   // false, true
+
+    @Option(name = "-isWkt1", usage = "输入目标数据几何列是否是WKT")
+    private String isWkt1 = "true";   // true, false
+
+    @Option(name = "-geometryFields1", usage = "输入目标数据几何列")
+    private String geometryFields1 = "-1";   // separate by ","
+
+    @Option(name = "-geometryType1", usage = "输入目标数据几何类型")
+    private String geometryType1 = "LineString";   // Polygon, LineString, Point
+
+    @Option(name = "-separator1", usage = "输入目标数据分隔符")
+    private String separator1 = "\t";
+
+    @Option(name = "-crs1", usage = "输入目标数据地理参考")
+    private String crs1 = "4326";   // 4326, 3857
+
+    @Option(name = "-charset1", usage = "输入目标数据字符集")
+    private String charset1 = "UTF-8";   // UTF-8, GBK
+
+    @Option(name = "-input2", usage = "输入范围数据路径", required = true)
+    private String input2;
+    /**
+     * The below is only for text file
+     */
+    @Option(name = "-hasHeader2", usage = "输入范围数据是否含有标题行")
+    private String hasHeader2 = "false";   // false, true
+
+    @Option(name = "-isWkt2", usage = "输入范围数据几何列是否是WKT")
+    private String isWkt2 = "true";   // true, false
+
+    @Option(name = "-geometryFields2", usage = "输入范围数据几何列")
+    private String geometryFields2 = "-1";   // separate by ","
+
+    @Option(name = "-geometryType2", usage = "输入目标数据几何类型")
+    private String geometryType2 = "LineString";   // Polygon, LineString, Point
+
+    @Option(name = "-separator2", usage = "输入范围数据分隔符")
+    private String separator2 = "\t";
+
+    @Option(name = "-crs2", usage = "输入范围数据地理参考")
+    private String crs2 = "4326";   // 4326, 3857
+
+    @Option(name = "-charset2", usage = "输入范围数据字符集")
+    private String charset2 = "UTF-8";   // UTF-8, GBK
+
+    public static UnionArgs initArgs(String[] args) {
+        UnionArgs mArgs = new UnionArgs();
+        CmdLineParser parser = new CmdLineParser(mArgs);
+        try {
+            parser.parseArgument(args);
+            return mArgs;
+        } catch (CmdLineException e) {
+            log.error(e.getLocalizedMessage());
+            return null;
+        }
+    }
+}
